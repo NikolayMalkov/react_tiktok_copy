c07cff1c740da92d59aeb9c37bef8d240ffdb74e
unknown <fcttmk@yandex.ru>
2021-06-04 15:17:25 +0700

    edit title

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div className="header">
 
         <div className="header_logotype">
-          <div className="header_logotext">GoWatch</div>
+          <div className="header_logotext">GoShare</div>
         </div>
 
         <div className="searchform">
